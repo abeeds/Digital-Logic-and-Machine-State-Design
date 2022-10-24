1aeebd98642ff9cef4756146a02b2bbb223cd5dc
Shahria A <105658920+abeeds@users.noreply.github.com>
2022-10-24 13:34:21 -0400

    Update lab_4_continuous.v

--- Lab 4/Part 1/lab_4_continuous.v
+++ Lab 4/Part 1/lab_4_continuous.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module lab4_continuous(
+module lab_4_continuous(
     input a0, a1, a2,
     output z0, z1, z2, z3, z4, z5, z6, z7
     );
