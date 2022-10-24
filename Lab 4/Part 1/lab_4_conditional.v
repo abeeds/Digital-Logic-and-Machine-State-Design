112c518aa5e4fe45cdc1a223f7507ad6e0d873f0
Shahria A <105658920+abeeds@users.noreply.github.com>
2022-10-24 13:45:40 -0400

    Update lab_4_conditional.v

--- Lab 4/Part 1/lab_4_conditional.v
+++ Lab 4/Part 1/lab_4_conditional.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module lab4_conditional(
+module lab_4_conditional(
     input a0, a1, a2,
     output z0, z1, z2, z3, z4, z5, z6, z7
     );
