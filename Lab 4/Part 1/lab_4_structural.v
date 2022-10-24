78f6c7701ddc13d281f983be26206c4025f3a88a
Shahria A <105658920+abeeds@users.noreply.github.com>
2022-10-24 13:46:23 -0400

    Update lab_4_structural.v

--- Lab 4/Part 1/lab_4_structural.v
+++ Lab 4/Part 1/lab_4_structural.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/14/2022 11:11:26 AM
-// Design Name: 
-// Module Name: Lab4
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 // Register - what you save values in
 // Wires - connects two components
