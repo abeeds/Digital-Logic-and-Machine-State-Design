9e60187c1c788130edfe641200b746c28e74f29f
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2022-10-28 18:02:56 -0400

    Create counter_tb.v

---
+++ Lab 4/Part 2/counter_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+
+
+module CounterC_tb( );
+    reg clk, F, P;
+    wire [3:0] out_count;
+    
+    CounterC ABC (.clk(clk), .F(F), .P(P), .out_count(out_count));
+    //lab_4_counter dut (.clk(clk), .out(out_count));
+    //lab_4_controlF dut (.clk(clk), .F(F), .out(out_count));
+    
+    always #10 clk = ~clk;
+    
+    initial begin
+        F <= 0; P <= 0; clk <= 0;
+        #50 F <= 1;
+        #50 F <= 0; P <= 1;
+        #50 P <= 1;
+       
+    end
+
+endmodule
